8cb3a56055cb6126f74452beacca207797029e19
Alukard <kirill1974111@gmail.com>
2025-03-20 15:59:57 +0100

    Caching and Linear Fibonacci Algorithn

--- 12_recursive.js
+++ 12_recursive.js
@@ -26,4 +26,46 @@ async function fibonacciNonBlocking(number){
     })
 }
 
-fibonacciNonBlocking(6).then(result => console.log("Fibonacci  number at position 6: ", result));
+fibonacciNonBlocking(6).then(result => console.log("Fibonacci Non-Blocking  number at position 6: ", result));
+
+
+// And in the end lest create Fibonacci algrorithm without recursion
+// with Linear Complexity
+const cach2 = new Map();
+async function createBreak(){
+    return new Promise((resolve, reject) => {
+        setImmediate(() => resolve());
+    })
+}
+async function linearFibonacci(number) {
+    let index = 0;
+    while(index <= number){
+        if(index === 0 || index === 1) cach2.set(index, index);
+        else cach2.set(index, cach2.get(index - 1) + cach2.get(index -2));
+        ++index;
+        await createBreak();
+    }
+    return cach2.get(number);
+}
+const linearResult = await linearFibonacci(40);
+console.log("Fibonacci Cached Linear number 40: ", linearResult);
+
+
+
+// // Now lets create Fibonacci algorithm using Caching
+const cach = new Map();
+
+async function cachFibonacciNonBlocking(number){
+    return new Promise((resolve, reject) => {
+        if(number === 0 || number === 1) return resolve(number);
+        if(cach.has(number)) return resolve(cach.get(number));
+        setImmediate(() => {
+            Promise.all([cachFibonacciNonBlocking(number - 1), cachFibonacciNonBlocking(number - 2)])
+                .then(([first, second]) => {
+                    cach.set(number, first + second);
+                    resolve(first + second);
+                });
+        })
+    })
+}
+cachFibonacciNonBlocking(6).then(result => console.log("Fibonacci Cached Non-Blocking number at position 6: ", result));
