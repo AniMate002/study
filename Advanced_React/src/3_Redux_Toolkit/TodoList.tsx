84404e1a583998eea4f19c0851098b96032487d5
Alukard <kirill1974111@gmail.com>
2025-03-28 00:57:56 +0100

    Async Redux-Toolkit + Typescript

--- Advanced_React/src/3_Redux_Toolkit/TodoList.tsx
+++ Advanced_React/src/3_Redux_Toolkit/TodoList.tsx
@@ -1,29 +1,36 @@
-import React from "react";
-import { removeTodo, toggleTodoComplete } from "../ReduxToolkitStore/todoSlice";
+import React, { useEffect } from "react";
+import {
+    deleteTodoAsync,
+    fetchTodos,
+    toggleCompletedTodoAsync,
+} from "../ReduxToolkitStore/todoSlice";
 import { useAppDispatch, useAppSelector } from "../ReduxToolkitStore/hooks";
 
 const TodoList = () => {
-    const { todos } = useAppSelector((store) => store.todos);
+    const { todos, error, isLoadingTodos } = useAppSelector(
+        (store) => store.todos
+    );
     const dispatch = useAppDispatch();
-    const handleDeleteTodo = (id: string) => dispatch(removeTodo(id));
-    const handleToggleCompletedTodo = (id: string) =>
-        dispatch(toggleTodoComplete(id));
+    const handleDeleteTodo = (id: number) => dispatch(deleteTodoAsync(id));
+    const handleToggleCompletedTodo = (id: number) =>
+        dispatch(toggleCompletedTodoAsync(id));
+
+    useEffect(() => {
+        dispatch(fetchTodos());
+    }, [dispatch]);
+
+    if (isLoadingTodos) return <h2>Loading...</h2>;
+    if (error) return <h2>Error: {error}</h2>;
     return (
         <div>
             <h3>Todo List:</h3>
             {todos.map((todo) => (
                 <div key={todo.id}>
-                    {todo.text} -
-                    <button
-                        onClick={() =>
-                            handleToggleCompletedTodo(todo.id.toString())
-                        }
-                    >
+                    {todo.title} -
+                    <button onClick={() => handleToggleCompletedTodo(todo.id)}>
                         {todo.completed ? "DONE" : "IN PROGRESS"}
                     </button>
-                    <button
-                        onClick={() => handleDeleteTodo(todo.id.toString())}
-                    >
+                    <button onClick={() => handleDeleteTodo(todo.id)}>
                         Delete
                     </button>
                 </div>
--- Advanced_React/src/ReduxToolkitStore/todoSlice.ts
+++ Advanced_React/src/ReduxToolkitStore/todoSlice.ts
@@ -1,21 +1,102 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+    createSlice,
+    PayloadAction,
+    createAsyncThunk,
+    AnyAction,
+} from "@reduxjs/toolkit";
 
 type Todo = {
-    id: string;
-    text: string;
+    id: number;
+    title: string;
     completed: boolean;
+    userId: number;
 };
 
 type InitialStateType = {
     todos: Array<Todo>;
+    isLoadingTodos: boolean;
+    error: string | null;
 };
 
 const initialState: InitialStateType = {
-    todos: [
-        { id: "4322432", text: "go to shop", completed: true },
-        { id: "7867878", text: "take pills", completed: false },
-    ],
+    todos: [],
+    isLoadingTodos: false,
+    error: null,
 };
+
+export const fetchTodos = createAsyncThunk<
+    Array<Todo>,
+    void,
+    { rejectValue: string }
+>("todos/fetchTodos", async (_, { rejectWithValue }) => {
+    try {
+        const res = await fetch(
+            "https://jsonplaceholder.typicode.com/todos?_limit=10"
+        );
+        console.log(res);
+        if (!res.ok) throw new Error(res.statusText);
+        return await res.json();
+    } catch (error) {
+        return rejectWithValue(
+            error instanceof Error ? error.message : "Unknown error"
+        );
+    }
+});
+
+export const deleteTodoAsync = createAsyncThunk<
+    void,
+    number,
+    { rejectValue: string }
+>("todos/deleteTodoAsync", async (id, { rejectWithValue, dispatch }) => {
+    try {
+        const res = await fetch(
+            `https://jsonplaceholder.typicode.com/todos/${id}`,
+            {
+                method: "DELETE",
+            }
+        );
+        if (!res.ok) throw new Error(res.statusText);
+        console.log("DELETED SUCCESSFULLY: ", id);
+        dispatch(removeTodo(id));
+    } catch (error) {
+        return rejectWithValue(
+            error instanceof Error ? error.message : "Unknown error"
+        );
+    }
+});
+
+export const toggleCompletedTodoAsync = createAsyncThunk<
+    void,
+    number,
+    { rejectValue: string; state: { todos: InitialStateType } }
+>(
+    "todos/toggleCompletedTodoAsync",
+    async (id: number, { rejectWithValue, dispatch, getState }) => {
+        try {
+            const todo = getState().todos.todos.find(
+                (todo: Todo) => todo.id === id
+            );
+            if (!todo) throw new Error("Todo State is empty");
+            const res = await fetch(
+                `https://jsonplaceholder.typicode.com/todos/${id}`,
+                {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ completed: !todo.completed }),
+                }
+            );
+            if (!res.ok) throw new Error(res.statusText);
+            console.log("UPDATED SUCCESSFULLY: ", id, !todo.completed);
+            dispatch(toggleTodoComplete(id));
+        } catch (error) {
+            return rejectWithValue(
+                error instanceof Error ? error.message : "Unknown error"
+            );
+        }
+    }
+);
 
 const todoSlice = createSlice({
     name: "todos",
@@ -23,24 +104,45 @@ const todoSlice = createSlice({
     reducers: {
         addTodo: function (state, action: PayloadAction<string>) {
             state.todos.push({
-                id: new Date().toISOString(),
-                text: action.payload,
+                id: state.todos.length,
+                title: action.payload,
                 completed: false,
+                userId: 1,
             });
         },
-        removeTodo: function (state, action: PayloadAction<string>) {
+        removeTodo: function (state, action: PayloadAction<number>) {
             state.todos = state.todos.filter(
                 (todo) => todo.id !== action.payload
             );
         },
-        toggleTodoComplete: function (state, action: PayloadAction<string>) {
+        toggleTodoComplete: function (state, action: PayloadAction<number>) {
             const foundTodo = state.todos.find(
                 (todo) => todo.id === action.payload
             );
             if (foundTodo) foundTodo.completed = !foundTodo.completed;
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchTodos.pending, (state, action) => {
+                state.isLoadingTodos = true;
+                state.error = null;
+            })
+            .addCase(fetchTodos.fulfilled, (state, action) => {
+                state.isLoadingTodos = false;
+                state.error = null;
+                state.todos = action.payload;
+            })
+            .addMatcher(isError, (state, action: PayloadAction<string>) => {
+                state.error = action.payload as string;
+                state.isLoadingTodos = false;
+            });
+    },
 });
 
 export default todoSlice.reducer;
 export const { addTodo, removeTodo, toggleTodoComplete } = todoSlice.actions;
+
+function isError(action: AnyAction) {
+    return action.type.endsWith("rejected");
+}
